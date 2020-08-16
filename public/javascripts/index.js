fccc1b78f762779138c69012dca53620abe7352d
Zaffere Shaffari <zaffere@Zafferes-MacBook-Pro.local>
2020-08-16 21:57:04 +0800

    Added the public Js file

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,9 +1,27 @@
-async function getThis() {
-  const here = await axios.get({
-    url:'http://localhost:5000/topic/CSS'
-  })
+// async function getThis() {
+//   const here = await axios({
+//     url:'http://localhost:5000/topic/HTML',
+//     method: 'GET'
+//   })
+//   console.log(here.data);
+//   const allData =
+//
+//   const div = document.body.createElement('div');
+//   div.innerHTML =
+//   document.body.appendChild(div)
+// }
+//
+// getThis();
 
-  console.log(here);
-}
+const findData = axios({
+  url:'http://localhost:5000/topic/HTML',
+  method: 'GET'
+});
 
-getThis();
+
+findData.then(res => {
+  console.log(res.data);
+  const div = document.createElement('p');
+  div.innerHTML = res.data[5].article;
+  document.body.appendChild(div);
+})
