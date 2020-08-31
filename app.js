1721f2cb26bfc02c72cf125a1e66f09bbe3c05c1
Zaffere Shaffari <zaffere@Zafferes-MacBook-Pro.local>
2020-08-31 19:37:19 +0800

    Added comment to app.js.

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/topic', topicRouter);
-app.use('/database', databaseApi);
+app.use('/database', databaseApi); // Only for REST
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
