57e57ca925d80ab2b7c4cf1453b36d25ca8633b2
ZAF07 <zaffere07@gmail.com>
2021-03-17 22:10:07 +0800

    Added dynamic year in sign up page

--- controllers/RootController.js
+++ controllers/RootController.js
@@ -5,8 +5,11 @@ exports.homePage = (req, res) => {
 };
 
 exports.signUp = (req, res) => {
+  const d = new Date();
+  const y = d.getFullYear();
   res.render('rootsTemplates/sign', {
-    title: 'Sign Up Page'
+    title: 'Sign Up Page',
+    year: y
   });
 };
 
